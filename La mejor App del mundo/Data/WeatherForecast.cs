427fe532ac91407849c6022cf0cd6b0b071b1c19
ASigness <angelasignes17@gmail.com>
2024-02-23 18:19:02 +0100

    Agregar archivos de proyecto.

---
+++ La mejor App del mundo/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace La_mejor_App_del_mundo.Data
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
